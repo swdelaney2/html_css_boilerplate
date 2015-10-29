0312c3e5f98798c6d3f4275f9a3a237c5ec76de6
Stephen Delaney <swdelaney2@gmail.com>
2015-10-29 16:58:16 -0500

    created great mad libs

--- scripts/app.js
+++ scripts/app.js
@@ -6,11 +6,30 @@ window.onload = function() {
   statusReport('Hey hey hey!');
 
   var username = prompt('What is your name?');
-  var mission = prompt('What is your mission?');
+  var gender = prompt('Are you male or female?');
+  var celeb = prompt('Name a celebrity you find attractive.');
+  var city = prompt('Name a foreign city.');
+  var adjective = prompt('List an adjective.');
+  var noun = prompt('List a noun.');
+  var skydive = confirm('Would you like to skydive?');
+
+  function pronoun(){
+    if (gender === 'male' || gender === 'Male') {
+    return 'he';
+  }  else {
+    return 'she';}
+  }
+
+  function danger(){
+    if (skydive == true) {
+    return 'I am totally going to go skydiving tomorrow.';
+  }  else {
+    return 'If only I had the courage to skydive.';}
+  }
 
   var contentContainer = document.getElementById('content')
 
-  contentContainer.innerHTML = username + ' is on a mission to ' + mission +'.'
+  contentContainer.innerHTML = username + ' looked in the mirror. "This is not how I expected things to turn out," ' + pronoun() + ' said. "I can\'t believe I am living in ' + city + ' and married to ' + celeb + '. I am a total ' + adjective + ' ' + noun + '." ' + danger()
 }
 
 function statusReport(message){
