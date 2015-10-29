79795edf0e3424cce1df1b689bcd6ee29c3b2799
Stephen Delaney <swdelaney2@gmail.com>
2015-10-29 16:15:25 -0500

    additional info

--- scripts/app.js
+++ scripts/app.js
@@ -4,6 +4,13 @@
 // Allows everything to load before alert happens
 window.onload = function() {
   statusReport('Hey hey hey!');
+
+  var username = prompt('What is your name?');
+  var mission = prompt('What is your mission?');
+
+  var contentContainer = document.getElementById('content')
+
+  contentContainer.innerHTML = username + ' is on a mission to ' + mission
 }
 
 function statusReport(message){
